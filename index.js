50ebfb73d3e6d7a310321829a33a3b7e9a3a2944
Yukina Ishiguro <68873109+uia-jp@users.noreply.github.com>
2022-02-07 13:07:36 -0500

    lab update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
+mongoose.connect('mongodb+srv://yukina:gbcmongoyukina@comp3123.ffyzn.mongodb.net/COMP3123?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(success => {
@@ -17,4 +17,4 @@ mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
 
 app.use(employeeRouter);
 
-app.listen(8081, () => { console.log('Server is running...') });
+app.listen(8080, () => { console.log('Server is running...') });
